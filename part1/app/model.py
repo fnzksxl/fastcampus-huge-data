a666c7d3a4654871b48542db3c13e61f62363a87
Taewon <71972587+fnzksxl@users.noreply.github.com>
2023-12-11 06:53:42 +0800

    Feat: DB Member Table 생성

--- part1/app/model.py
+++ part1/app/model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, DateTime, func
+from sqlalchemy import Column, Integer, DateTime, func, String
+from app.database import Base
 
 
 # 테이블의 기본이 되는 인덱스, 생성시각, 변경시각을 사전 정의해두자
@@ -6,3 +7,15 @@ class BaseMin:
     id = Column(Integer, primary_key=True, index=True)
     created_at = Column(DateTime, nullable=False, default=func.now())
     updated_at = Column(DateTime, nullable=False, default=func.now(), onupdate=func.now())
+
+
+class Member(Base, BaseMin):
+    __tablename__ = "member"
+
+    email = Column(String(25), nullable=False, unique=True)
+    nickname = Column(String(10), nullable=False)
+    birthday = Column(DateTime, nullable=False)
+
+    # Member 객체를 Dictionary 형태로 변환할 수 있음
+    def as_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
