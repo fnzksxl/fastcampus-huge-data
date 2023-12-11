f5e0ef0c64e7a3752b49e17ca02d6ce42147be8c
Taewon <71972587+fnzksxl@users.noreply.github.com>
2023-12-11 13:42:40 +0800

    Refactor: 닉네임 길이 검증 기능 함수화

--- part1/app/api/v1/member/service.py
+++ part1/app/api/v1/member/service.py
@@ -3,14 +3,18 @@ from fastapi import HTTPException, status
 from app import model
 
 
-async def memberSave(memberInfo, db):
+async def validateNickname(nickname):
     try:
-        assert len(memberInfo.nickname) < 10, "닉네임 최대 길이를 초과했습니다."
+        assert len(nickname) < 10, "닉네임 최대 길이를 초과했습니다."
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail=f"{e} Occured. While asserting 'memberSave'",
         )
+
+
+async def memberSave(memberInfo, db):
+    validateNickname(memberInfo.nickname)
     try:
         memberInfoRow = model.Member(**memberInfo.dict())
         db.add(memberInfoRow)
